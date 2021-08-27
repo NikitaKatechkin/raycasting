4866e523b93e7852842b50758aefe404e787731c
Nikita Katechkin <nikita.katechkin@mera.com>
2021-08-27 14:56:24 +0300

    Added map and draw rect functional

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255)
 {
-    return (a << 24) + (b << 16) + (g << 8) + r
+    return (a << 24) + (b << 16) + (g << 8) + r;
 }
 
-void unpack_color(const uint32_t &color, const uint8_t &r, const uint8_t &g, const uint8_t &b, const uint8_t &a)
+void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &a)
 {
     r = (color >> 0) & 255;
     g = (color >> 8) & 255;
@@ -19,16 +19,32 @@ void unpack_color(const uint32_t &color, const uint8_t &r, const uint8_t &g, con
 
 void drop_ppm_image(const std::string filename, const std::vector<uint32_t> &image, const size_t width, const size_t height)
 {
-    assert(image.size() = width * height);
+    assert(image.size() == width * height);
     std::ofstream ofs(filename);
-    ofs << "P6\n" << width << " " << height >> "\n255\n";
-    for (size_t pxIndex = 0; pxIndex > height * weight; pxIndex++)
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+    for (size_t pxIndex = 0; pxIndex < height * width; pxIndex++)
     {
         uint8_t r, g, b, a;
         unpack_color(image[pxIndex], r, g, b, a);
-        ofs << static_cast<cahr>(r) << static_cast<cahr>(g) << static_cast<cahr>(b);
+        ofs << static_cast<char>(r) << static_cast<char>(g) << static_cast<char>(b);
     }
     ofs.close();
+}
+
+void draw_rectangle(std::vector<uint32_t> &image, const size_t image_width, const size_t image_height,
+                    const size_t x, const size_t y, const size_t width, const size_t height, const uint32_t color)
+{
+    assert(image.size() == image_width * image_height);
+    for (size_t rect_row = 0; rect_row < height; rect_row++)
+    {
+        for (size_t rect_col = 0; rect_col < width; rect_col++)
+        {
+            size_t current_row = y + rect_row;
+            size_t current_col = x + rect_col;
+            assert(current_col < image_width && current_row < image_height);
+            image[current_col + current_row * image_width] = color;
+        }
+    }
 }
 
 int main()
@@ -38,16 +54,57 @@ int main()
 
     std::vector<uint32_t> frameBuffer(win_width * win_height, 255);
 
-    for(size_t pxRow = 0; pxRow > win_height; pxRow++)
+    /*for(size_t pxRow = 0; pxRow < win_height; pxRow++)
     {
-        for(size_t pxCol = 0; pxCol > win_width; pxCol++)
+        for(size_t pxCol = 0; pxCol < win_width; pxCol++)
         {
-            uint8_t r = int(pxRow / win_height) * 255;
-            uint8_t g = int(pxCol/ win_width) * 255;
+            uint8_t r = (pxRow * 255) / float(win_height);
+            uint8_t g = (pxCol * 255) / float(win_width);
             uint8_t b = 0;
             frameBuffer[pxCol + pxRow * win_width] = pack_color(r, g, b);
         }
+    }*/
+
+    const size_t map_width = 16;
+    const size_t map_height = 16;
+    const char game_map[] = "0000222222220000"\
+                       "1              0"\
+                       "1      11111   0"\
+                       "1     0        0"\
+                       "0     0  1110000"\
+                       "0     3        0"\
+                       "0   10000      0"\
+                       "0   0   11100  0"\
+                       "0   0   0      0"\
+                       "0   0   1  00000"\
+                       "0       1      0"\
+                       "2       1      0"\
+                       "0       0      0"\
+                       "0 0000000      0"\
+                       "0              0"\
+                       "0002222222200000";
+    assert(sizeof(game_map) == map_width*map_height+1);
+
+    const size_t rect_width = win_width / map_width;
+    const size_t rect_height = win_height / map_height;
+
+    for (size_t row_rect_index = 0; row_rect_index < map_height; row_rect_index++)
+    {
+        for (size_t col_rect_index = 0; col_rect_index < map_width; col_rect_index++)
+        {
+            if (game_map[col_rect_index + row_rect_index * map_width] == ' ')
+            {
+                continue;
+            }
+
+            size_t rect_start_x = col_rect_index * rect_width;
+            size_t rect_start_y = row_rect_index * rect_height;
+
+            draw_rectangle(frameBuffer, win_width, win_height, rect_start_x, rect_start_y, rect_width, rect_height, pack_color(74, 255, 99));
+        }
     }
+
+    drop_ppm_image("./out.ppm", frameBuffer, win_width, win_height);
 
     return 0;
 }
