97687701f08e9c513a0141825c09fbb63ee4e780
Nikita Katechkin <nikita.katechkin@mera.com>
2021-08-28 01:29:34 +0300

    Wrapped drawing first view into function

--- main.cpp
+++ main.cpp
@@ -10,6 +10,34 @@
 
 #define PI 3.14159
 
+void draw_first_view(Phys_size winSize, std::vector<uint32_t>& frameBuffer, Player& player, Game_map& game_map)
+{
+    const size_t rect_width = winSize.width / (game_map.m_sizeInTile.width * 2);
+    const size_t rect_height = winSize.height / game_map.m_sizeInTile.height;
+
+    for (size_t i = 0; i < winSize.width / 2; i++)
+    {
+        float angle = (player.getView().angle - player.getView().field_of_view / 2) + i * (player.getView().field_of_view / float(winSize.width / 2));
+        for(float distance = 0; distance < 20; distance += 0.05)
+        {
+            float current_x = player.getCoords().x_coordinate + distance * cos(angle);
+            float current_y = player.getCoords().y_coordinate + distance * sin(angle);
+
+            size_t px_x = current_x * rect_width;
+            size_t px_y = current_y * rect_height;
+            frameBuffer[px_x + px_y * winSize.width] = pack_color(160, 160, 160);
+
+            if (game_map.game_scheme[int(current_x)+ int(current_y)*game_map.m_sizeInTile.width] != ' ')
+            {
+                size_t column_height = winSize.height / distance;
+                draw_rectangle(frameBuffer, winSize.width, winSize.height, (winSize.width/2 + i), (winSize.height / 2 - column_height / 2),
+                               1, column_height, pack_color(0, 255, 255));
+                break;
+            }
+        }
+    }
+}
+
 int main()
 {
     const size_t win_width = 1024;
@@ -29,27 +57,7 @@ int main()
     draw_rectangle(frameBuffer, win_width, win_height, player.getCoords().x_coordinate * rect_width,
                    player.getCoords().y_coordinate * rect_height, player.getSize().width, player.getSize().height, pack_color(255, 255, 255));
 
-    for (size_t i = 0; i < win_width / 2; i++)
-    {
-        float angle = (player.getView().angle - player.getView().field_of_view / 2) + i * (player.getView().field_of_view / float(win_width / 2));
-        for(float distance = 0; distance < 20; distance += 0.05)
-        {
-            float current_x = player.getCoords().x_coordinate + distance * cos(angle);
-            float current_y = player.getCoords().y_coordinate + distance * sin(angle);
-
-            size_t px_x = current_x * rect_width;
-            size_t px_y = current_y * rect_height;
-            frameBuffer[px_x + px_y * win_width] = pack_color(160, 160, 160);
-
-            if (game_map.game_scheme[int(current_x)+ int(current_y)*game_map.m_sizeInTile.width] != ' ')
-            {
-                size_t column_height = win_height / distance;
-                draw_rectangle(frameBuffer, win_width, win_height, (win_width/2 + i), (win_height / 2 - column_height / 2), 1, column_height, pack_color(0, 255, 255));
-                break;
-            }
-        }
-    }
-
+    draw_first_view(phys_size(win_width, win_height), frameBuffer, player, game_map);
 
     drop_ppm_image("./out.ppm", frameBuffer, win_width, win_height);
 
